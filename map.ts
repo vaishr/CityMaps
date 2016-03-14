44ee489213b698c4e907fbfe03ebb9c379af200a
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-03-13 20:31:49 -0700

    add popups for markers

--- map.ts
+++ map.ts
@@ -1,45 +1,35 @@
 /// <reference path="./typings/tsd.d.ts"/>
 
+const geojson: any[] = [];
+
+function templateGeo(title: string, lat: number, lng: number, description: string): any {
+  return {
+    "type": "Feature",
+    "geometry": {
+      "type": "Point",
+      "coordinates": [lat, lng]
+    },
+    "properties": {
+      "title": title,
+      "description": description,
+      "marker-color": "#3ca0d3",
+      "marker-size": "large"
+    }
+  };
+}
 function init() {
-  const places = {};
   const input = (<HTMLInputElement>document.getElementById("placeInput"));
   const autocomplete = new google.maps.places.Autocomplete(input);
   const map = L.mapbox.map("map", "mapbox.emerald");
   map.setView([37.783, -122.455], 14);
   const myLayer = L.mapbox.featureLayer().addTo(map);
-  const geojson = [
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-77.031952, 38.913184]
-      },
-      "properties": {
-        "title": "Mapbox DC",
-        "description": "1714 14th St NW, Washington DC",
-        "marker-color": "#3ca0d3",
-        "marker-size": "large",
-        "marker-symbol": "rocket"
-      }
-    },
-    {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [-122.413682, 37.775408]
-      },
-      "properties": {
-        "title": "Mapbox SF",
-        "description": "155 9th St, San Francisco",
-        "marker-color": "#63b6e5",
-        "marker-size": "large",
-        "marker-symbol": "rocket"
-      }
-    }
-  ];
-  L.mapbox.featureLayer().setGeoJSON(geojson);
-  //  map.scrollWheelZoom.disable();
-
+  const addMarker = (lati: number, long: number, description: string, name: string) => {
+    const newMarker = L.marker([lati, long]);
+    const content = `<h2>${name}</h2><h3>${description}</h3>`;
+    newMarker.addTo(map);
+    newMarker.bindPopup(content);
+    map.scrollWheelZoom.disable();
+  };
 
   const placeNote = $("#placeNote");
   $("#placeInput").on("input", () => {
@@ -50,14 +40,16 @@ function init() {
     } else {
       placeNote.attr("disabled", "disabled");
     }
-    console.log(val.length);
   });
   $("#add").on("click", () => {
     const place = autocomplete.getPlace();
     const lat = place.geometry.location.lat();
     const lng = place.geometry.location.lng();
-    L.marker([lat, lng]).addTo(map);
-    places[{ lat: lat, lng: lng }.toString()] = placeNote.val();
+    const note = placeNote.val();
+    console.log("place", place);
+    geojson.push(templateGeo(place.name, lat, lng, note));
+    console.log("geoJson", geojson);
+    addMarker(lat, lng, place.name, note);
   });
 }
 L.mapbox.accessToken = "pk.eyJ1IjoidmFpcmVkZHkxMSIsImEiOiJhYjVmNmY2MWQ3MmFiNThkZjBiZTA1MzdkNTg3NTJhZiJ9.6YTxS5LbsOmXzVcUWzgE7w";
