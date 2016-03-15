742138cfc4287ada8da3be60c36a3bf6cd4d631f
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-03-14 23:18:56 -0700

    add map controller

--- map.ts
+++ map.ts
@@ -1,6 +1,12 @@
 /// <reference path="./typings/tsd.d.ts"/>
 
+
 const geojson: any[] = [];
+let autocomplete;
+let addMarker;
+let map;
+let input;
+let myLayer;
 
 function templateGeo(title: string, lat: number, lng: number, description: string): any {
   return {
@@ -17,40 +23,43 @@ function templateGeo(title: string, lat: number, lng: number, description: strin
     }
   };
 }
-function init() {
-  const input = (<HTMLInputElement>document.getElementById("placeInput"));
-  const autocomplete = new google.maps.places.Autocomplete(input);
-  const map = L.mapbox.map("map", "mapbox.emerald");
-  map.setView([37.783, -122.455], 14);
-  const myLayer = L.mapbox.featureLayer().addTo(map);
-  const addMarker = (lati: number, long: number, description: string, name: string) => {
-    const newMarker = L.marker([lati, long]);
-    const content = `<h2>${name}</h2><h3>${description}</h3>`;
-    newMarker.addTo(map);
-    newMarker.bindPopup(content);
-    map.scrollWheelZoom.disable();
-  };
 
-  const placeNote = $("#placeNote");
-  $("#placeInput").on("input", () => {
-    const placeInput = $("#placeInput");
-    const val = placeInput.val();
-    if (val.length > 0) {
-      placeNote.removeAttr("disabled");
-    } else {
-      placeNote.attr("disabled", "disabled");
-    }
-  });
-  $("#add").on("click", () => {
+function init() {
+  input = (<HTMLInputElement>document.getElementById("placeInput"));
+  autocomplete = new google.maps.places.Autocomplete(input);
+  map = L.mapbox.map("map", "mapbox.emerald");
+  map.setView([37.783, -122.455], 14);
+  myLayer = L.mapbox.featureLayer().addTo(map);
+};
+
+L.mapbox.accessToken = "pk.eyJ1IjoidmFpcmVkZHkxMSIsImEiOiJhYjVmNmY2MWQ3MmFiNThkZjBiZTA1MzdkNTg3NTJhZiJ9.6YTxS5LbsOmXzVcUWzgE7w";
+google.maps.event.addDomListener(window, "load", init);
+
+let app = angular.module("app", ["firebase"]);
+
+app.controller("MapCtrl", ["$scope", "$timeout", function($scope, $timeout) {
+  $scope.submit = function() {
+    const note = $scope.placeNote;
     const place = autocomplete.getPlace();
     const lat = place.geometry.location.lat();
     const lng = place.geometry.location.lng();
-    const note = placeNote.val();
     console.log("place", place);
+    console.log($scope.placeNote);
     geojson.push(templateGeo(place.name, lat, lng, note));
     console.log("geoJson", geojson);
+    addMarker = (lati: number, long: number, name: string, description: string) => {
+      const newMarker = L.marker([lati, long]);
+      const content = `<h2>${name}</h2><h3>${description}</h3>`;
+      newMarker.addTo(map);
+      newMarker.bindPopup(content);
+      map.scrollWheelZoom.disable();
+    };
     addMarker(lat, lng, place.name, note);
+  };
+}]);
+
+angular.element(document).ready(() => {
+  angular.bootstrap(document, [app.name], {
+    strictDi: true
   });
-}
-L.mapbox.accessToken = "pk.eyJ1IjoidmFpcmVkZHkxMSIsImEiOiJhYjVmNmY2MWQ3MmFiNThkZjBiZTA1MzdkNTg3NTJhZiJ9.6YTxS5LbsOmXzVcUWzgE7w";
-google.maps.event.addDomListener(window, "load", init);
+});
